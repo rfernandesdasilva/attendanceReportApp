828827c3cdfccbeffaf7871b7dc45b66af0c894c
rfernandesdasilva <121926908+rfernandesdasilva@users.noreply.github.com>
2023-11-13 23:27:38 -0500

    Authentication API calls for the User to login and create account. New models for these data objects!

---
+++ attendanceReport/Models/Student.swift
@@ -0,0 +1,8 @@
+//
+//  Student.swift
+//  attendanceReport
+//
+//  Created by Rafael Fernandes da Silva on 11/13/23.
+//
+
+import Foundation
---
+++ attendanceReport/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  attendanceReport
+//
+//  Created by Rafael Fernandes da Silva on 11/13/23.
+//
+
+import Foundation
